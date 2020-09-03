23cc2affb6ac45c6d9adafe0d011731174a99250
Stanislav-g <65065137+Stanislav-g@users.noreply.github.com>
2020-09-03 19:11:22 +0200

    Update rrrnbot.py

--- rrrnbot.py
+++ rrrnbot.py
@@ -13,7 +13,7 @@ from random import randint, choice, choices
 import io
 import random as r
 import ffmpeg
-import PyNaCl
+from discord import voice 
 
 
 client = commands.Bot( command_prefix = '-')
